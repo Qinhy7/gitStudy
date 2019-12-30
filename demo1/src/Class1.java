d02a8c2eb63583cb601c8d9513432f511a5806a0
qinhy <1927371079@qq.com>
2019-12-30 10:06:31 +0800

    Class1提交

---
+++ demo1/src/Class1.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：qinhy
+ * @date ：Created in 2019/12/30 0030 10:05
+ */
+public class Class1 {
+
+    public static void main(String[] args){
+        System.out.println("-------");
+    }
+
+}
