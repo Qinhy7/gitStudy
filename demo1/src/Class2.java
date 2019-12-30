efb4926a129467b23ec159e3f2dc1368069124aa
Qinhy7 <41883855+Qinhy7@users.noreply.github.com>
2019-12-30 10:09:57 +0800

    Create Class2.java

---
+++ demo1/src/Class2.java
@@ -0,0 +1,11 @@
+/**
+ * @author ：qinhy
+ * @date ：Created in 2019/12/30 0030 10:05
+ */
+public class Class2 {
+
+    public static void main(String[] args){
+        System.out.println("Class2  第一次提交");
+    }
+
+}
