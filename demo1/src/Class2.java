8c8c3c1ced0d8cde6092c5ebff1d0f6255ebddaa
Qinhy7 <41883855+Qinhy7@users.noreply.github.com>
2019-12-30 10:26:32 +0800

    Update Class2.java
    到底

--- demo1/src/Class2.java
+++ demo1/src/Class2.java
@@ -7,7 +7,7 @@ public class Class2 {
     public static void main(String[] args){
         System.out.println("Class2  第一次提交");
         System.out.println("Class2  第而次提交");
-
+        System.out.println("Class2  再次提交");
     }
 
 }
